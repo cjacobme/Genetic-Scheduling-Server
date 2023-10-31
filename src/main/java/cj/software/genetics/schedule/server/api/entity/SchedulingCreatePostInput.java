2b46767bfd398099e7bbb983f7a01e72d7c4a60d
cjacobme <cjacobme@aol.com>
2023-10-31 23:06:51 +0100

    added request post input

---
+++ src/main/java/cj/software/genetics/schedule/server/api/entity/SchedulingCreatePostInput.java
@@ -0,0 +1,71 @@
+package cj.software.genetics.schedule.server.api.entity;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.io.Serial;
+import java.io.Serializable;
+
+public class SchedulingCreatePostInput implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @NotNull
+    @Valid
+    private SchedulingProblem schedulingProblem;
+
+    @NotNull
+    @Valid
+    private SolutionSetup solutionSetup;
+
+    @NotNull
+    @Valid
+    private BreedingSetup breedingSetup;
+
+    private SchedulingCreatePostInput() {
+    }
+
+    public SchedulingProblem getSchedulingProblem() {
+        return schedulingProblem;
+    }
+
+    public SolutionSetup getSolutionSetup() {
+        return solutionSetup;
+    }
+
+    public BreedingSetup getBreedingSetup() {
+        return breedingSetup;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        protected SchedulingCreatePostInput instance;
+
+        protected Builder() {
+            instance = new SchedulingCreatePostInput();
+        }
+
+        public SchedulingCreatePostInput build() {
+            SchedulingCreatePostInput result = instance;
+            instance = null;
+            return result;
+        }
+
+        public Builder withSchedulingProblem(SchedulingProblem schedulingProblem) {
+            instance.schedulingProblem = schedulingProblem;
+            return this;
+        }
+
+        public Builder withSolutionSetup(SolutionSetup solutionSetup) {
+            instance.solutionSetup = solutionSetup;
+            return this;
+        }
+
+        public Builder withBreedingSetup(BreedingSetup breedingSetup) {
+            instance.breedingSetup = breedingSetup;
+            return this;
+        }
+    }
+}
