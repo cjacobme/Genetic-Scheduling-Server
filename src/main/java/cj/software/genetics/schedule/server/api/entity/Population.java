2aadbe3854c60589cb818e2376518d4bafdef0f1
cjacobme <cjacobme@aol.com>
2023-11-01 20:51:53 +0100

    added Population

---
+++ src/main/java/cj/software/genetics/schedule/server/api/entity/Population.java
@@ -0,0 +1,51 @@
+package cj.software.genetics.schedule.server.api.entity;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Population implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @NotEmpty
+    private final List<@NotNull @Valid Solution> solutions = new ArrayList<>();
+
+    private Population() {
+    }
+
+    public List<Solution> getSolutions() {
+        return Collections.unmodifiableList(solutions);
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        protected Population instance;
+
+        protected Builder() {
+            instance = new Population();
+        }
+
+        public Population build() {
+            Population result = instance;
+            instance = null;
+            return result;
+        }
+
+        public Builder withSolutions(List<Solution> solutions) {
+            instance.solutions.clear();
+            if (solutions != null) {
+                instance.solutions.addAll(solutions);
+            }
+            return this;
+        }
+    }
+}
