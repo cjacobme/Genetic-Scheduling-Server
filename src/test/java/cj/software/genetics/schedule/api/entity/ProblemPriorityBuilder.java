b0ca3bc32a6664a2b1a7e13bd5455e2817317393
cjacobme <cjacobme@aol.com>
2023-11-22 22:47:30 +0100

    split time and unit into 2 fields

--- src/test/java/cj/software/genetics/schedule/api/entity/ProblemPriorityBuilder.java
+++ src/test/java/cj/software/genetics/schedule/api/entity/ProblemPriorityBuilder.java
@@ -1,6 +1,5 @@
 package cj.software.genetics.schedule.api.entity;
 
-import java.time.Duration;
 import java.util.Set;
 
 public class ProblemPriorityBuilder extends ProblemPriority.Builder {
@@ -9,7 +8,7 @@ public class ProblemPriorityBuilder extends ProblemPriority.Builder {
                 .withSlotCount(20)
                 .withTasks(Set.of(
                         new TaskBuilder().withIdentifier(1).build(),
-                        new TaskBuilder().withIdentifier(2).withDuration(Duration.ofSeconds(20)).build(),
-                        new TaskBuilder().withIdentifier(3).withDuration(Duration.ofMinutes(1)).build()));
+                        new TaskBuilder().withIdentifier(2).withDuration(TimeWithUnit.ofSeconds(20)).build(),
+                        new TaskBuilder().withIdentifier(3).withDuration(TimeWithUnit.ofMinutes(1)).build()));
     }
 }
