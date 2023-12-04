641d51871a47c4e27c8f0b01330f8d30795e8bda
cjacobme <cjacobme@aol.com>
2023-12-04 21:37:19 +0100

    repaired test

--- src/test/java/cj/software/genetics/schedule/server/api/controller/SchedulingControllerImplTest.java
+++ src/test/java/cj/software/genetics/schedule/server/api/controller/SchedulingControllerImplTest.java
@@ -1,16 +1,16 @@
 package cj.software.genetics.schedule.server.api.controller;
 
 import cj.software.genetics.schedule.api.entity.BreedPostInput;
+import cj.software.genetics.schedule.api.entity.BreedPostInputBuilder;
 import cj.software.genetics.schedule.api.entity.BreedPostOutput;
 import cj.software.genetics.schedule.api.entity.Population;
+import cj.software.genetics.schedule.api.entity.PopulationBuilder;
 import cj.software.genetics.schedule.api.entity.SchedulingCreatePostInput;
+import cj.software.genetics.schedule.api.entity.SchedulingCreatePostInputBuilder;
 import cj.software.genetics.schedule.api.entity.SchedulingCreatePostOutput;
 import cj.software.genetics.schedule.api.entity.SchedulingProblem;
 import cj.software.genetics.schedule.api.entity.SolutionSetup;
 import cj.software.genetics.schedule.api.exception.SlotOccupiedException;
-import cj.software.genetics.schedule.api.entity.BreedPostInputBuilder;
-import cj.software.genetics.schedule.api.entity.PopulationBuilder;
-import cj.software.genetics.schedule.api.entity.SchedulingCreatePostInputBuilder;
 import cj.software.genetics.schedule.server.util.Breeder;
 import cj.software.genetics.schedule.server.util.PopulationService;
 import org.junit.jupiter.api.Test;
@@ -62,16 +62,20 @@ class SchedulingControllerImplTest {
 
     @Test
     void breed() throws SlotOccupiedException {
-        BreedPostInput breedPostInput = new BreedPostInputBuilder().build();
+        BreedPostInput breedPostInput = new BreedPostInputBuilder().withNumSteps(3).build();
         Population previous = breedPostInput.getPopulation();
-        Population breeded = Population.builder().build();
+        Population breeded1 = Population.builder().build();
+        Population breeded2 = Population.builder().build();
+        Population breeded3 = Population.builder().build();
 
-        when(breeder.step(previous, 2, 5, 0.356)).thenReturn(breeded);
+        when(breeder.step(previous, 2, 5, 0.356)).thenReturn(breeded1);
+        when(breeder.step(breeded1, 2, 5, 0.356)).thenReturn(breeded2);
+        when(breeder.step(breeded2, 2, 5, 0.356)).thenReturn(breeded3);
 
         BreedPostOutput returned = schedulingControllerImpl.breed(breedPostInput);
 
         verify(breeder).step(previous, 2, 5, 0.356);
         assertThat(returned).as("returned").isNotNull();
-        assertThat(returned.getPopulation()).as("returned population").isSameAs(breeded);
+        assertThat(returned.getPopulation()).as("returned population").isSameAs(breeded3);
     }
 }
